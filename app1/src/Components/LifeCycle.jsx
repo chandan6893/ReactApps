1f362f22dc31b6206cfbfe85108ce1ee90de379e
Chandan Kumar <krc6893@gmail.com>
2022-09-21 20:30:45 +0530

    Increment

---
+++ app1/src/Components/LifeCycle.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+export class LifeCycle extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 1,
+        };
+    }
+
+    incrementCount = () => {
+        this.setState((prev) => ({
+            count:prev.count + 1,
+        }))
+    }
+
+    render() {
+        return (
+            <>
+            <h1>count - {this.state.count}</h1>
+            <button onClick={this.incrementCount} >Increment</button>
+            {/* {this.state.count > 6 && <h1>We Will Rule the World</h1> } */}
+            {/* {this.state.count > 4 ? <h3>We Will Rise Again</h3> : <h4>Never Again</h4> } */}
+            </>
+        )
+    }
+}
