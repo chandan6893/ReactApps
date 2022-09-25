76f8f0471b3e7766cfcd1439ef4ccde72ac47a37
Chandan Kumar <krc6893@gmail.com>
2022-09-25 14:11:05 +0530

    SecPush

--- todos/src/components/ToDoList.jsx
+++ todos/src/components/ToDoList.jsx
@@ -8,15 +8,37 @@ export class ToDoList extends Component{
         }
     }
 
+    onChangeHandler = (event) =>{
+        this.setState({
+            term:event.target.value,
+        })
+    }
+
+    onFormHandler = (event) =>{
+        event.preventDefault();
+        this.setState({
+            term:'',
+            items:[...this.state.items,this.state.term]
+        })
+    }
 
     render(){
-        return(
-            <>
+        return (
+          <>
             <div>
-                <input type="text" />
-                <button>Add Item</button>
+              <form>
+                <div>
+                  <input
+                    onChange={this.onChangeHandler}
+                    type="text"
+                    value={this.state.term}
+                    placeholder="enter term"
+                  />
+                  <button onClick={this.onFormHandler}>Add Item</button>
+                </div>
+              </form>
             </div>
-            </>
-        )
+          </>
+        );
     }
 }
