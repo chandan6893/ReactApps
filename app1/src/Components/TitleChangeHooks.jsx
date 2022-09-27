c0cbb772075adec603d9d887151cedc8b67b8eac
Chandan Kumar <krc6893@gmail.com>
2022-09-27 19:13:52 +0530

    Title  change By Hooks and TitleChange By Class component

--- app1/src/Components/TitleChangeHooks.jsx
+++ app1/src/Components/TitleChangeHooks.jsx
@@ -1,13 +1,25 @@
 import React, { useState , useEffect} from "react";
 export function TitleChangeHooks(){
    const [count, setCount] = useState(0);
+   const [name, setName]   = useState();
+
    useEffect(() =>{
+    console.log("count Changed")
     document.title = `${count} times clicked`;
-   });
+
+    // for the first time load (first rendering) useEffect wiil get called.
+
+    // FOR EACH RE-RENDERING PROCESS useEffect() get called.  
+   },[count]);
+
+   useEffect(()=>{
+    console.log("name Changed")
+   },[name])
 
    return(
     <>
     <div className="titleChangeHooks">
+        <input type="text" value={name} onChange = {(e)=>setName(e.target.value)} />
         <h3>Count : {count}</h3>
         <button onClick={()=> setCount(count + 1)}>Click Here Hooks</button>
     </div>
