4dfb33ba647b690901d1825948c7cb2828ab01c1
Chandan Kumar <krc6893@gmail.com>
2022-10-07 02:11:24 +0530

    app

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -4,7 +4,27 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     
+      <div className="container">
+        <h1>ToDo App</h1>
+        <form className="todoForm">
+          <input type="text" />
+          <button>Add</button>
+        </form>
+
+        <ul>
+          <li>
+            <span>Learning React</span>
+            <button>Edit</button>
+            <button>Delete</button>
+          </li>
+
+          <li>
+            <span>Learning React</span>
+            <button>Edit</button>
+            <button>Delete</button>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
