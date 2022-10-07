d954f16dac7128c5981acd649092c73b06dee1e7
Chandan Kumar <krc6893@gmail.com>
2022-10-07 11:10:06 +0530

    reactjs

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,28 +1,34 @@
-
+import React, { useState } from "react";
 import './App.css';
 
 function App() {
+  const [todo, setTodo] = useState("");
+  const [todos,setTodos]= useState([]);
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    
+    if(todo !== ""){
+      setTodos([...todos,{ id: `${todo} - ${Date.now()}`, todo }]);
+    }
+  }
   return (
     <div className="App">
       <div className="container">
         <h1>ToDo App</h1>
-        <form className="todoForm">
-          <input type="text" />
-          <button>Add</button>
+        <form className="todoForm" onSubmit={handleSubmit}>
+          <input type="text" onChange={(e) => setTodo(e.target.value)} />
+          <button type="submit">Add</button>
         </form>
 
-        <ul>
-          <li>
-            <span>Learning React</span>
-            <button>Edit</button>
-            <button>Delete</button>
-          </li>
-
-          <li>
-            <span>Learning React</span>
-            <button>Edit</button>
-            <button>Delete</button>
-          </li>
+        <ul className="allTodos">
+          {todos.map((t) => (
+            <li className="singleTodo">
+              <span className="todoText">Learning React</span>
+              <button>Edit</button>
+              <button>Delete</button>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
