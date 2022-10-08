90c3f9d5e7f263afd644ad3995dd465a4fcdabb3
Chandan Kumar <krc6893@gmail.com>
2022-10-08 22:17:45 +0530

    css

--- login-form/src/components/Form.jsx
+++ login-form/src/components/Form.jsx
@@ -1,14 +1,31 @@
-import React from "react";
+import React,{ useState } from "react";
+
 import "./form.css";
 
 export function Form(){
 
+  const [id, setId] = useState("");
+  const [pwd,setPwd] = useState("");
+  const [submit, setSubmit] = useState();
+
     return (
       <div className="container">
         <form className="form">
-          <input type="text" placeholder="Username" />
-          <input type="password" placeholder="Passwurd" />
-          <button>submit</button>
+          <input
+            className="inputA"
+            type="text"
+            placeholder="UserName...."
+            value={id}
+            onChange={(e) => setId(console.log(e.target.value))}
+          />
+          <input
+            className="inputB"
+            type="password"
+            placeholder="Password...."
+            value={pwd}
+            onChange={(e) => setPwd(e.target.value)}
+          />
+          <button type="submit" className="submitButton" >Login</button>
         </form>
       </div>
     );
