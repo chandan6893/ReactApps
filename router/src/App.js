4fbe80eaa3992e20db82081f3c7934d0f5373834
Chandan Kumar <krc6893@gmail.com>
2022-10-10 20:32:18 +0530

    Router

---
+++ router/src/App.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Content from "./components/Content"
+import { Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <>
+    <Header />
+      <Routes>
+        <Route element={<Home />} path="/" />
+        <Route element={<About />} path="/about" />
+        <Route element={<Contact />} path="/contact" />
+        <Route element={<Content />} path="/content" />
+     </Routes>
+    </>
+  );
+}
+
+export default App;
