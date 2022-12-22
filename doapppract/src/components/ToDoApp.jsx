51e1334f4fc1b8bae9db8d1652f45443809533b7
Chandan Kumar <krc6893@gmail.com>
2022-12-22 10:42:39 +0530

    app

--- doapppract/src/components/ToDoApp.jsx
+++ doapppract/src/components/ToDoApp.jsx
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function ToDoApp() {
+    
+    const [data,setData]=useState("");
+    const [item, setItem] = useState([]);
+
+    const handleClick=()=>{
+        setItem([...item,data]);
+        setData("")
+        // console.log(item)
+    }
   return (
-    <>ToDoApp</>
+    <>
+    
+        <input type="text" value={data} onChange={(e)=>setData(e.target.value)} />
+        <button onClick={handleClick} >ADD ITEM</button>
+        <div>{item.map((i)=>{
+            return <h1 key={Math.random(new Date())}>{i}</h1>;
+        })}</div>
+    
+    </>
   )
 }
 
