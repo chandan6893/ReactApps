0eaaec8aa5102b66b57e201f6d70a99514d3e8c5
Chandan Kumar <krc6893@gmail.com>
2023-01-13 19:35:52 +0530

    movie page grid

--- movieapp/src/components/Movies.jsx
+++ movieapp/src/components/Movies.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import { useGlobalContext } from './Context';
+import {NavLink} from "react-router-dom"
 
 function Movies() {
   const {movie} =useGlobalContext()
   return (
-    <>
-      {movie.map((currEle)=>{
-        return <div>
-          <h2>{currEle.Title}</h2>
-        </div>
-      })}
-    </>
+    <section className='movie-page' >
+      <div className='grid grid-4-col' >{movie.map((currMve)=>{
+        const { Title, imdbID, Poster } = currMve;
+        return (
+          <NavLink to={`movie/${imdbID}`} key={imdbID}>
+            <div className='card'>
+              <div className="card-info">
+                <h2>{Title}</h2>
+                <img src={Poster} alt="" />
+              </div>
+            </div>
+          </NavLink>
+        );
+      })}</div>
+      
+    </section>
   )
 }
 
