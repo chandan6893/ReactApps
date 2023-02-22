588f74c3c4793b68e938ad6b9ea09b574697b5c3
Chandan Kumar <krc6893@gmail.com>
2023-02-23 01:21:04 +0530

    easyshopt

---
+++ easyshop/src/components/Header.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+      <div className="headerLogo">
+        <h1 className="logo">EasyShop</h1>
+      </div>
+
+      <div className="headerSearch">
+        <input className="searchInput" type="text" placeholder="Search..." />
+      </div>
+
+      <div className="headerNav">
+        <div className="nav__item">
+          <span className="navItemLineOne">Hello Guest</span>
+          <span className="navItemLineTwo">Sign In</span>
+        </div>
+
+        <div className="nav__item">
+          <span className="navItemLineOne">Your</span>
+          <span className="navItemLineTwo">Shop</span>
+        </div>
+
+        <div className="nav__item">
+          <span className="navItemLineOne cart">
+            <i class="fa-solid fa-cart-shopping"></i>
+          </span>
+          <span className="navItemLineTwo">cart</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header
