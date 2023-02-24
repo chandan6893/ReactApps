2992ab6e0472428e4ef8669a310bafffe923bae1
Chandan Kumar <krc6893@gmail.com>
2023-02-24 15:57:36 +0530

    product and home page

--- easyshop/src/components/Home.jsx
+++ easyshop/src/components/Home.jsx
@@ -12,18 +12,74 @@ function Home() {
           alt=""
         />
         <div className="home__row">
-          <Products />
-          <Products />
+          <Products
+            id="12321341"
+            title="Kellogg's Muesli 21% Fruit, Nut & Seeds 750g | 5 Grains, High in Vitamins B1, B2, B3, B6, Folate, Source of Protein and Fibre, Multigrain Breakfast Cereal"
+            price={372.0}
+            rating={5}
+            image="https://m.media-amazon.com/images/I/91i9rdecBvL._SX679_.jpg"
+          />
+          <Products
+            id="1115544"
+            title="Fire TV Cube | Hands-free streaming device with Alexa | 4K Ultra HD"
+            price={8949}
+            rating={4.7}
+            image="https://m.media-amazon.com/images/I/51AuUshPluL._SX425_.jpg"
+          />
+          <Products
+            id="545646544"
+            title="Ariel Matic Top Load Detergent Washing Powder - 2 kg"
+            price={396.0}
+            rating={4}
+            image="https://m.media-amazon.com/images/I/71G9Nf0q4bL._SY450_.jpg"
+          />
         </div>
         <div className="home__row">
-          <Products />
-          <Products />
-          <Products />
+          <Products
+            className="prodOne"
+            id="022787853"
+            title="Brother DCP-L2541DW Multi-Function Monochrome Laser Printer with Wi-Fi, Network & Auto Duplex Printing"
+            price={21699}
+            rating={3.8}
+            image="https://m.media-amazon.com/images/I/71JDCbasgaL._SX569_.jpg"
+          />
+          <Products
+            id="90154648"
+            title="Tecno POP 6 Pro (Peaceful Blue, 2GB RAM,32GB Storage) | Front Flash | 8MP Dual Camera"
+            price={6299}
+            rating={3.6}
+            image="https://m.media-amazon.com/images/I/81qiSz8eTCL._SX569_.jpg"
+          />
+          <Products
+            id="68584510"
+            title="Redmi Smart Band Pro SportsWatch- 3.73 cm (1.47) Large AMOLED Display, Always On Display, Continuous Sleep, HR, Stress and SPO2 Monitoring, 110+ Sports Modes, 5ATM, 14 Days Battery Life, Black"
+            price={1999}
+            rating={4}
+            image="https://m.media-amazon.com/images/I/417DDJ5EPHL._SX300_SY300_QL70_FMwebp_.jpg"
+          />
         </div>
         <div className="home__row">
-          <Products />
-          <Products />
-          <Products />
+          <Products
+            id="351579"
+            title="House Of Silko Royal Traditional Banarasi Brocade patola Animal Motif Cushion Cover 16x16 Set of 2"
+            price={585}
+            rating={4.8}
+            image="https://m.media-amazon.com/images/I/51jye8tcpEL._SY300_SX300_QL70_FMwebp_.jpg"
+          />
+          <Products
+            id="1155779"
+            title="LookMark Men's Poly Cotton Digital Printed Stitched Half Sleeve Shirt"
+            price={339}
+            rating={5}
+            image="https://m.media-amazon.com/images/I/71cFpnm0b6S._UX679_.jpg"
+          />
+          <Products
+            id="587143577"
+            title="Ray-Ban Boy and Girl Square Sunglasses"
+            price={3890}
+            rating={4.9}
+            image="https://m.media-amazon.com/images/I/61yYcYexdWL._UX679_.jpg"
+          />
         </div>
       </div>
     </div>
--- easyshop/src/components/Products.jsx
+++ easyshop/src/components/Products.jsx
@@ -1,26 +1,24 @@
 import React from 'react';
 import "./Products.css"
 
-function Products() {
+function Products({id, title, price, rating, image}) {
   return (
-    <div>
-      <div className="product">
-        <div className="product__info">
-          <p>
-            ASUS Vivo AiO V222, 4 core Intel Pentium Silver J5040, All-in-One
-            Desktop (8GB/256GB SSD/Integrated Graphics/Windows 11/Black/4.8 kg),
-            V222GAK-BA034W
-          </p>
-          <p className="product__price">Rs29,990</p>
-          <div className="product__rating">⭐⭐</div>
+    <>
+      <div className="productParent">
+        <div className="product">
+          <div className="product__info">
+            <p>{title}</p>
+            <p className="product__price">
+              <small>Rs</small>
+              <strong>{price}</strong>
+            </p>
+            <div className="product__rating">⭐⭐</div>
+          </div>
+          <img src={image} alt="" />
+          <button>add to cart</button>
         </div>
-        <img
-          src="https://m.media-amazon.com/images/I/71kDCavI6JS._SL1500_.jpg"
-          alt=""
-        />
-        <button>add to cart</button>
       </div>
-    </div>
+    </>
   );
 }
 
