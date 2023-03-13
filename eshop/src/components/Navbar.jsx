d64afecdb1533553e94afc4dff95524f062d8661
Chandan Kumar <krc6893@gmail.com>
2023-03-13 16:07:52 +0530

    eshob navbar

---
+++ eshop/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { AppBar, Toolbar, Typography } from "@mui/material";
+
+const Navbar = () => {
+  return (
+    <AppBar>
+        <Toolbar>
+            <Typography variant='h4' >eShop</Typography>
+        </Toolbar>
+    </AppBar>
+  )
+}
+export default Navbar;
+
