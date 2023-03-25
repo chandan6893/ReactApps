44cf874e8225610dbed23c9059a6ab3da5220092
Chandan Kumar <krc6893@gmail.com>
2023-03-25 16:09:23 +0530

    react-carousel

--- react-carousel/src/components/Carousel.jsx
+++ react-carousel/src/components/Carousel.jsx
@@ -1,20 +1,28 @@
-import React from 'react';
+import React,{ useState } from 'react';
 import "./carousel.css";
 
 export const Carousel = ({slides}) => {
+    const [slideImg,setSlideImg] = useState(0);
+    const prevSlide = ()=>{
+        setSlideImg(slideImg === 0 ? slides.length-1 : slideImg-1);
+    }
+
+    const nextSlide = () =>{
+        setSlideImg(slideImg===slides.length-1 ? 0 : slideImg+1)
+    }
     // console.log(slides)
   return (
     <div className="carousel">
-      <i className="fa-solid fa-chevron-left fa-beat arrow arrow-left "></i>
+      <i className="fa-solid fa-chevron-left fa-beat arrow arrow-left " onClick={prevSlide}></i>
       {slides.map((img, index) => {
-        return <img src={img.src} alt={img.alt} className="slideImg" />;
+        return <img src={img.src} alt={img.alt} className={slideImg === index ? "slideImg" :"slide-hidden"} />;
       })}
-      <i className="fa-solid fa-chevron-right fa-beat arrow arrow-right "></i>
+      <i className="fa-solid fa-chevron-right fa-beat arrow arrow-right " onClick={nextSlide}></i>
       <span className='indicators'>
         {slides.map((_,index)=>{
-            return <button className='indicator' key={index} onClick={null} ></button>
+            return <button className={slideImg === index ? "indicator" : "indicator indicator-inactive"} key={index} onClick={()=>setSlideImg(index)} ></button>
         })}
       </span>
     </div>
   );
-}
+} 
