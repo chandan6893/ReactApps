ccbaeaea981a1d40095cef624bf405f13e1211d7
Chandan Kumar <krc6893@gmail.com>
2023-04-12 23:31:28 +0530

    add

--- reactToDoPract/src/App.jsx
+++ reactToDoPract/src/App.jsx
@@ -3,16 +3,25 @@ import './App.css'
 
 function App() {
 
-
+  const [enterItem,setEnterItem] = useState("");
+  const [data,setData] = useState([])
+  
+const handleAddItems =()=>{
+  if(enterItem.length !==0 ){
+    setData([...data, enterItem]);
+  }
+  setEnterItem(" ");
+}
+console.log("DATA==>",data,enterItem.length)
   return (
-    <>
+    
       <div className="App">
         <div className='inputAndAddBtn'>
-          <input type="text" placeholder="Add Items..." />
-          <button>Add</button>
+          <input type="text" placeholder="Add Items..." value={enterItem} onChange={(e)=>setEnterItem(e.target.value)} />
+          <button onClick={handleAddItems} >Add</button>
         </div>
       </div>
-    </>
+    
   );
 }
 
